fa9d92847de208f0776718425a0f9b34b2871171
CurtisSquire <45086802+CurtisSquire@users.noreply.github.com>
2018-12-14 13:39:22 -0500

    Add files via upload

---
+++ Quit.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quit : MonoBehaviour {
+    // a method that exits the game when the Quit button is pressed. 
+public void Quitgame()
+    {
+        Application.Quit(); 
+    }
+}
